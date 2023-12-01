f73deb05a922f45f3c25f9fa6592de555f63e041
davinharding <davinlharding@gmail.com>
2023-11-30 19:01:21 -0500

    fixed logo

--- src/components/elements/MoralisLogo/MoralisLogo.tsx
+++ src/components/elements/MoralisLogo/MoralisLogo.tsx
@@ -1,19 +1,21 @@
-import { useColorMode } from '@chakra-ui/react';
+import { useColorMode, Flex, Box, Text } from '@chakra-ui/react';
 import Image from 'next/image';
 
 const MoralisLogo = () => {
   const { colorMode } = useColorMode();
 
   return (
-    <div>
-      <Image
-        src={colorMode === 'dark' ? '/pixlverse_logo.png' : '/pixlverse_logo.png'}
-        height={45}
-        width={150}
-        alt="Moralis"
-      />
-      <div>Pixlfolio</div>
-    </div>
+    <Flex align="center">
+      <Box>
+        <Image
+          src={colorMode === 'dark' ? '/pixlverse_logo.png' : '/pixlverse_logo.png'}
+          height={45}
+          width={150}
+          alt="Moralis"
+        />
+      </Box>
+      <Text ml="3">Pixlfolio</Text>
+    </Flex>
   );
 };
 
